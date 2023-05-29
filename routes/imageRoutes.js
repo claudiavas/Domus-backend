d661f1cc43fe89d8acc8a9dff89b5b519ead72c8
Esteban <esteban@prueba.com>
2023-05-29 20:04:33 +0200

    save changes merge

---
+++ routes/imageRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+ratingController = require('../controllers/imageController')
+
+
+router.get('/:imageId?', imageController.getImage)
+router.post('/', imageController.addImage)
+// router.delete('/:imageId', imageController.deleteImage);
+// router.put('/:imageId', imageController.updateImage);
+// router.delete('/:imageId/permanent', imageController.permanentDelete);
+
+module.exports = router;
