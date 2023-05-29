69ad60b72c3b55f825f1279b0449d28b1e25b56c
Esteban <esteban@prueba.com>
2023-05-29 19:34:47 +0200

    add image in routes/index

--- routes/index.js
+++ routes/index.js
@@ -15,4 +15,10 @@ router.post('/', ratingController.addRating)
 // router.put('/:ratingId', ratingController.updateRating);
 // router.delete('/:ratingId/permanent', ratingController.permanentDelete);
 
+router.get('/:imageId?', imageController.getImage)
+router.post('/', imageController.addImage)
+// router.delete('/:imageId', imageController.deleteImage);
+// router.put('/:imageId', imageController.updateImage);
+// router.delete('/:imageId/permanent', imageController.permanentDelete);
+
 module.exports = router;
