3f2e37af4e4015daef2c5757fd1af9d61d2b06bb
Antonio Zurano Blazquez <azurano@outlook.es>
2023-05-30 21:32:04 +0200

    finish code y probe post /register user

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -2,11 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 
-
+let mySecret = process.env.mysecret;
 
 /* register */
 router.post('/register', function(req, res, next) {
-  res.send('respond with a resource');
+  const { name, email, password  } = req.body;
+  
+  const token = jwt.sign({name, email, password}, mySecret, {expiresIn: '1h'});
+
+  res.status(200).json({ result: 'sucess'}, token);
 });
 
 router.post('/login', function(req, res, next) {
