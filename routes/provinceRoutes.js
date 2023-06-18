8147b5297c829569c94a6e13a85146f57f1be290
Claudia Vasquez <claudia.vasquez.as@gmail.com>
2023-06-18 18:29:17 +0200

    not a table anymore

--- routes/provinceRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const provinceController = require('../controllers/provinceController');
-
-router.get('/:provinceId?', provinceController.getProvince);
-router.post('/', provinceController.addProvince);
-
-module.exports = router;
