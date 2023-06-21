f9364bc241cd360c848436a988577aebdb828b97
Claudia Vasquez <claudia.vasquez.as@gmail.com>
2023-06-21 18:04:32 +0200

    capital letter in route correcter

--- routes/realEstateRoutes.js
+++ routes/realEstateRoutes.js
@@ -5,7 +5,7 @@ const realEstateController = require('../controllers/realEstateController');
 router.get('/:realEstateId?', realEstateController.getRealEstate);
 router.post('/', realEstateController.addRealEstate);
 router.delete('/:realEstateId?', realEstateController.deleteRealEstate);
-router.put('/:RealEstateId', realEstateController.updateRealEstate);
+router.put('/:realEstateId', realEstateController.updateRealEstate);
 router.delete('/:houseId/permanent', realEstateController.permanentDeleteRealEstate);
 
 module.exports = router;
