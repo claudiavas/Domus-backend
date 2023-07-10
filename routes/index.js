cc882c6d7c08677c27b2b84da092aea2ec89ccec
Claudia Vasquez <claudia.vasquez.as@gmail.com>
2023-07-10 22:59:44 +0200

    deleting unused files

--- routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const generalRouter = express.Router();
-
-const userRouter = require('./userRoutes')
-const { jwtMiddleware, authRouter} = require("../security/jwt")
-
-module.exports = generalRouter;
--- routes/resetPasswordRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const  resetPasswordController  = require('../controllers/resetPasswordController');
-
-// Ruta para restablecer la contraseña
-router.post('/resetpassword', resetPasswordController.resetPassword);
-
-module.exports = router;
-
