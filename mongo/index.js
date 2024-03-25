4a930dc1e433af6ec1a36af4689bf7d670d95126
Claudia Vasquez <claudia.vasquez.as@gmail.com>
2024-03-25 02:50:59 +0100

    update connection string mongodb

--- mongo/index.js
+++ mongo/index.js
@@ -12,7 +12,6 @@ const databaseName = process.env.DATABASE_NAME || '27017';
 const databaseUser = process.env.DATABASE_USER;
 const databasePassword = process.env.DATABASE_PASSWORD;
 const databaseURL = "mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASSWORD + "@" + process.env.DB_SERVER + "/" + process.env.DB_NAME + "?retryWrites=true&w=majority";
- 
 
 
 //if we have a full mogourl defined in the .env then use it to start an online conection
